4f8dcd2d4ae395c85229d2eafbb082825fcc5706
ms0223900 <ms0223900@gmail.com>
2020-12-23 23:05:25 +0800

    refactor: refactor heap tree

--- practices/chapter7_heap/python/ch_7_heap_tree.py
+++ practices/chapter7_heap/python/ch_7_heap_tree.py
@@ -8,32 +8,10 @@ class Heap_tree():
     return index * 2 + 1
   def get_child_right_node_index(self, index):
     return index * 2 + 2
+
   def get_parent_node_index(self, index):
     return (index - 1) // 2
-
-  def data_down_change(self, index=0):
-    _index = index
-    # 對調節點
-    while self.get_child_right_node_index(_index) <= self.size:
-      min_index = self.get_min_index(_index)
-      # 如果該節點比該節點下的子節點最小還"大"
-      if self.heap[_index] > self.heap[min_index]:
-        # 對調
-        self.heap[_index], self.heap[min_index] = self.heap[min_index], self.heap[_index]
-      _index = min_index
-    pass
-
-  def data_up_change(self, index=0):
-    _index = index
-    # 對調節點
-    while self.get_parent_node_index(_index) >= 0:
-      parent_node_index = self.get_parent_node_index(_index)
-      if self.heap[_index] < self.heap[parent_node_index]:
-        self.heap[_index], self.heap[parent_node_index] = self.heap[parent_node_index], self.heap[_index]
-      _index = parent_node_index
-    pass
-  
-  def get_min_index(self, index):
+  def get_child_node_min_index(self, index):
     # 找該節點的右邊子節點(等同node.right)
     # 如果找不到，則給該節點的左子節點
     child_left_node_index = self.get_child_left_node_index(index)
@@ -47,6 +25,31 @@ class Heap_tree():
         return child_left_node_index
         pass
       return child_right_node_index
+
+  def data_down_change(self, index=0):
+    _index = index
+    # 對調節點
+    while self.get_child_right_node_index(_index) <= self.size:
+      min_index = self.get_child_node_min_index(_index)
+      # 如果該節點比該節點下的子節點最小還"大"
+      if self.heap[_index] > self.heap[min_index]:
+        # 對調
+        self.heap[_index], self.heap[min_index] = self.heap[min_index], self.heap[_index]
+      # 繼續往下找
+      _index = min_index
+    pass
+
+  def data_up_change(self, index=0):
+    _index = index
+    while self.get_parent_node_index(_index) >= 0:
+      parent_node_index = self.get_parent_node_index(_index)
+      # 如果該節點比父節點還小
+      if self.heap[_index] < self.heap[parent_node_index]:
+        # 對調節點
+        self.heap[_index], self.heap[parent_node_index] = self.heap[parent_node_index], self.heap[_index]
+      # 繼續往上找
+      _index = parent_node_index
+    pass
 
   def create_tree(self, list):
     self.heap = list
